08f9d24006d980e939a3b0fdc9a775277c301de9
Cristian Dominguez <cristian.eng3@gmail.com>
2020-10-05 20:36:04 -0300

    Renombrando un metodo

--- modulo-1.py
+++ modulo-1.py
@@ -1,4 +1,6 @@
 import modulos
-
+# Si quisiera renombrar a este modulo, es tan facil como declarar:
+# import modulos as xs
+# de esta forma, en vez de utilizar "modulos.saludo()" utilizaría "xs.saludo()"
 print(modulos.mascotas)
 modulos.saludo('Cristian')
